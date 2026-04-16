6b4568333f8b6f09cba2c47d8ae9d7c1070bdebb
wermej <143026706+wermej@users.noreply.github.com>
2026-04-16 14:51:33 -0700

    document dataset structure

--- sql/exploration.sql
+++ sql/exploration.sql
@@ -5,3 +5,21 @@
 -- What is conversion rate actually based on?
 -- Are leads and conversions related?
 -- Which fields are unreliable?
+
+-- Dataset: Google Ads (uncleaned)
+-- Table name: google_ads.master_table
+
+-- Columns observed:
+-- ad_id
+-- campaign_name
+-- clicks
+-- impressions
+-- cost
+-- leads
+-- conversions
+-- conversion_rate
+-- sale_amount
+-- ad_date
+-- location
+-- device
+-- keyword
