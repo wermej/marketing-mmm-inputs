f947fe47a20bddc7a22dcbe52a1ef56c41a5bf41
wermej <143026706+wermej@users.noreply.github.com>
2026-04-16 14:38:51 -0700

    Update exploration.sql

---
+++ sql/exploration.sql
@@ -0,0 +1,7 @@
+-- Exploring Google Ads dataset
+-- Goal: understand data quality and relationships between metrics
+
+-- Questions:
+-- What is conversion rate actually based on?
+-- Are leads and conversions related?
+-- Which fields are unreliable?
